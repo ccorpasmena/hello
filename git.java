a708ae9f83480e4a3a0da07a7ce9112432531ec7
ccorpasmena <ccorpasmena@gmail.com>
2024-01-05 00:01:56 +0100

    Este es mi tercer commit

--- git.java
+++ git.java
@@ -3,6 +3,6 @@ package hello;
 public class git {
     
     public static void main(String[] args) {        
-        System.out.println("Aprendiendo GIT");
+        System.out.println("Estoy aprendiendo GIT");
     }
 }
--- hello.java
+++ hello.java
@@ -3,6 +3,6 @@ package hello;
 public class hello {
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo");        
+        System.out.println("Estoy aprendiendo Java");        
     }
 }
