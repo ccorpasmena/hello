4a72dbb8783f9fd68f3477cdf3745d8ade55243f
ccorpasmena <ccorpasmena@gmail.com>
2024-01-09 18:38:27 +0100

    Se añade la informacion del comando stash

--- login.java
+++ login.java
@@ -4,6 +4,7 @@ public class login {
     public static void main(String[] args) {
         
         System.out.println("Aprendiendo a hacer login");
+        System.out.println("Estamos probando el comando stash");
     }    
     
 }
